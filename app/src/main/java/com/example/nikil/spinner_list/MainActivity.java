d9195e26f3eb4facef5bd0896068ebbad56791e0
nikilsss <karanjitnikil@gmail.com>
2016-08-16 07:33:51 +0545

    spinner

--- app/src/main/java/com/example/nikil/spinner_list/MainActivity.java
+++ app/src/main/java/com/example/nikil/spinner_list/MainActivity.java
@@ -12,21 +12,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner= (Spinner)findViewById(R.id.spinner);
-        adapter=ArrayAdapter.createFromResource(this,R.array.username,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // adapter=ArrayAdapter.createFromResource(this,R.array.username,android.R.layout.simple_spinner_item);
+       // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //spinner.setAdapter(adapter);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.username));
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + "Selected", Toast.LENGTH_SHORT).show();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
